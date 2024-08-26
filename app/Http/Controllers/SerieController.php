4b06192781420811b5b5ccf21a15bbb951947589
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-08-26 13:55:19 -0300

    Refactor: I modify how routes are accessed to a simpler form and simplified the logic for saving a new series

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -18,52 +18,34 @@ class SerieController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('series.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $serie = new Serie();
-        $serie->name = $request->input('name');
-        $serie->save();
+        Serie::create($request->all());
         return redirect('/series');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
         //
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\SerieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-Route::get('/series', [SerieController::class, 'index']);
-Route::get('/series/create', [SerieController::class, 'create']);
-Route::post('/series', [SerieController::class, 'store']);
+Route::resource('/series', SerieController::class);
